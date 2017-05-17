b36710c5c8ffe390f76823b450f77bf8624d2134
Lohith Dwaraka <ldwaraka@godaddy.com>
2017-05-16 21:58:16 -0700

    category based search in maps

--- routes/maps.js
+++ routes/maps.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('maps');
+    res.render('maps',{ category: req.query.category });
 });
 
 module.exports = router;
